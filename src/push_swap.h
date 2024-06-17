8cd43169e30320743232243ac1da1028d6a2a5dc
yinhong <yinhong@student.42tokyo.jp>
2024-06-17 13:56:28 +0900

    add t_stack struct, modify linked list functions to new types accordingly

--- src/push_swap.h
+++ src/push_swap.h
@@ -1,12 +1,30 @@
 #ifndef PUSH_SWAP_H
-#define PUSH_SWAP_H
+# define PUSH_SWAP_H
 
-#include "../libft/libft.h"
-#include "../ft_printf/ft_printf.h"
+# include "../ft_printf/ft_printf.h"
+# include "../libft/libft.h"
+# define TRUE 1
+# define FALSE 0
 
-void del(void *content);
-t_list *create_stack(int arr_size);
-void ft_initialize_stack(int arr_size, int *arr, t_list *stack);
-void print_stack(t_list *stack);
+typedef struct s_stack
+{
+	int				value;
+	int				index;
+	struct s_stack	*next;
+}					t_stack;
+
+t_stack				*create_stack(int arr_size);
+void				ft_initialize_stack(int arr_size, int *arr, t_stack *stack);
+void				print_stack(t_stack *stack);
+void				sort(t_stack *stack_a);
+void				sa(t_stack *a);
+void				ra(t_stack **a);
+t_stack				*stack_last(t_stack *lst);
+void				stack_add_front(t_stack **lst, t_stack *new);
+void				stack_add_back(t_stack **lst, t_stack *new);
+void				stack_delone(t_stack *lst);
+void				stack_clear(t_stack **lst);
+int					stack_size(t_stack *lst);
+t_stack				*stack_new(int value);
 
 #endif
