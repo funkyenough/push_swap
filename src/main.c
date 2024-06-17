28feb42e78f6c1246358c2798ed66cc1faf35223
yinhong <yinhong@student.42tokyo.jp>
2024-06-17 13:58:29 +0900

    add has_duplicate check

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 #include "push_swap.h"
 
-void del(void *content)
+void	del(void *content)
 {
 	free(content);
 }
@@ -22,56 +22,78 @@ int	str_count(const char *s, char c)
 	return (wordcount);
 }
 
-int *parse_arg_two(char *argv, int *arr_size)
+int	*parse_arg_two(char *argv, int *arr_size)
 {
-	char **chararr;
-	int *intarr;
-	int i;
+	char	**chararr;
+	int		*intarr;
+	int		i;
 
 	i = 0;
 	*arr_size = str_count(argv, ' ');
 	chararr = ft_split(argv, ' ');
 	if (!chararr)
-		return NULL;
+		return (NULL);
 	intarr = (int *)malloc(sizeof(int) * (*arr_size));
-
 	while (i < *arr_size)
 	{
 		intarr[i] = ft_atoi(chararr[i]);
 		i++;
 	}
-	return intarr;
+	return (intarr);
 }
 
-int *parse_all_args(int argc, char **argv)
+int	*parse_all_args(int argc, char **argv)
 {
-	int *intarr;
-	int i;
+	int	*intarr;
+	int	i;
 
 	intarr = (int *)malloc(sizeof(int) * (argc - 1));
 	if (!intarr)
-		return NULL;
+		return (NULL);
 	i = 0;
 	while (i < argc - 1)
 	{
 		intarr[i] = ft_atoi(argv[i + 1]);
 		i++;
 	}
-	return intarr;
+	return (intarr);
 }
+
+int	has_duplicates(int arr_size, int *intarr)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < arr_size)
+	{
+		j = i + 1;
+		while (j < arr_size)
+		{
+			if (intarr[i] == intarr[j])
+				return (TRUE);
+			j++;
+		}
+		i++;
+	}
+	return (FALSE);
+}
+
+// After sort, the first node of the stack should have the smallest number,
+// and the last node should have the largest.
 
 int	main(int argc, char **argv)
 {
 	int *intarr;
 	int arr_size;
-	t_list *stack;
+	t_stack *stack;
 
 	arr_size = 0;
 	intarr = NULL;
 	if (argc < 2)
 	{
 		ft_printf("Error");
-		return 1;
+		return (1);
 	}
 	if (argc == 2)
 		intarr = parse_arg_two(argv[1], &arr_size);
@@ -81,11 +103,19 @@ int	main(int argc, char **argv)
 		intarr = parse_all_args(argc, argv);
 	}
 
+	if (has_duplicates(arr_size, intarr))
+	{
+		ft_printf("Array has duplicates.");
+		return (1);
+	}
 	// for (int i = 0; i < arr_size; i++)
 	// 	ft_printf("%d\n", intarr[i]);
 
 	stack = create_stack(arr_size);
 	ft_initialize_stack(arr_size, intarr, stack);
 	print_stack(stack);
-	return 0;
+	sort(stack);
+	print_stack(stack);
+	ft_printf("main executed\n");
+	return (0);
 }
