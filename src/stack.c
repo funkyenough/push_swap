2a4478863ece1691d5927fc091b752826c637da8
yinhong <yinhong@student.42tokyo.jp>
2024-06-17 13:58:10 +0900

    add stack specific functions, and refactor

--- src/stack.c
+++ src/stack.c
@@ -1,47 +1,48 @@
 #include "push_swap.h"
 
-t_list *create_stack(int arr_size)
+t_stack	*create_stack(int arr_size)
 {
-	t_list *root;
-	t_list *head;
-	t_list *new_node;
+	t_stack	*root;
+	t_stack	*head;
+	t_stack	*new_node;
 
-	root = ft_lstnew((int *)malloc(sizeof(int)));
-	if (!root->content)
-		return NULL;
+	root = stack_new(0);
+	if (!root)
+		return (NULL);
 	head = root;
 	new_node = NULL;
 	while (arr_size-- > 1)
 	{
-		new_node = ft_lstnew((int *)malloc(sizeof(int)));
-		if (!new_node->content)
+		new_node = stack_new(0);
+		if (!new_node)
 		{
-			ft_lstclear(&root, del);
-			return NULL;
+			stack_clear(&root);
+			return (NULL);
 		}
-		ft_lstadd_front(&head, new_node);
+		stack_add_front(&head, new_node);
 		head = new_node;
 	}
-	return head;
+	return (head);
 }
 // The first element is at the top of the stack,
 // meaning that the last node has the first element of the array
-void ft_initialize_stack(int arr_size, int *arr, t_list *stack)
+void	ft_initialize_stack(int arr_size, int *arr, t_stack *stack)
 {
 	arr += (arr_size - 1);
-	while(stack)
+	while (stack)
 	{
-		*((int*)stack->content) = *arr;
+		stack->value = *arr;
 		stack = stack->next;
 		arr--;
 	}
 }
 
-void print_stack(t_list *stack)
+void	print_stack(t_stack *stack)
 {
+	ft_printf("Address of the stack is: %p\n", stack);
 	while (stack)
 	{
-		ft_printf("%d\n", *((int *)stack->content));
+		ft_printf("%d\n", stack->value);
 		stack = stack->next;
 	}
 }
---
+++ src/stackft.c
@@ -0,0 +1,71 @@
+#include "./push_swap.h"
+
+void	stack_add_back(t_stack **lst, t_stack *new)
+{
+	if (*lst == NULL)
+		*lst = new;
+	else if (lst != NULL && new != NULL)
+		stack_last(*lst)->next = new;
+}
+
+void	stack_add_front(t_stack **lst, t_stack *new)
+{
+	new->next = *lst;
+	*lst = new;
+}
+
+void	stack_delone(t_stack *lst)
+{
+	if (!lst)
+		return ;
+	free(lst);
+}
+
+t_stack	*stack_last(t_stack *lst)
+{
+	if (lst != NULL)
+	{
+		while (lst->next != NULL)
+			lst = lst->next;
+	}
+	return (lst);
+}
+
+void	stack_clear(t_stack **lst)
+{
+	t_stack	*next_node;
+
+	if (lst)
+	{
+		while (*lst)
+		{
+			next_node = (*lst)->next;
+			stack_delone(*lst);
+			*lst = next_node;
+		}
+	}
+}
+
+int	stack_size(t_stack *lst)
+{
+	int	len;
+
+	len = 0;
+	while (lst)
+	{
+		len++;
+		lst = lst->next;
+	}
+	return (len);
+}
+
+t_stack	*stack_new(int value)
+{
+	t_stack *node;
+	node = (t_stack *)malloc(sizeof(t_stack));
+	if (!node)
+		return (NULL);
+	node->value = value;
+	node->next = NULL;
+	return (node);
+}
