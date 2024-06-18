f93dc8a3999f0bf92c3fcbc42e2e1b88053239e0
yinhong <yinhong@student.42tokyo.jp>
2024-06-18 14:43:27 +0900

    cleanup + add leak detector

--- src/main.c
+++ src/main.c
@@ -5,24 +5,27 @@
 
 int	main(int argc, char **argv)
 {
-	int *intarr;
-	int arr_size;
-	t_stack *stack;
+	int		*intarr;
+	int		arr_size;
+	t_stack	*stack;
 
 	arr_size = 0;
 	intarr = NULL;
-
 	intarr = parse_input(argc, argv, intarr, &arr_size);
 	if (has_duplicates(arr_size, intarr))
 	{
-		ft_printf("Error: Array has duplicates.\n");
+		// ft_printf("Error: Array has duplicates.\n");
 		return (1);
 	}
-
 	stack = create_stack(arr_size);
-	ft_initialize_stack(arr_size, intarr, stack);
-	print_stack(stack);
-	sort(stack);
-	print_stack(stack);
+	ft_initialize_stack(intarr, stack);
+	sort(&stack);
+	stack_clear(&stack);
+	free(intarr);
 	return (0);
+}
+
+__attribute__((destructor)) static void destructor()
+{
+	system("leaks -q push_swap");
 }
